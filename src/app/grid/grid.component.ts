a552ac4427a1154bfab6d21d8ec4212fe6503320
Teki <teki@iMac-27-2017.local>
2020-08-02 04:46:24 +0900

    add play function

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GuardsCheckEnd } from '@angular/router';
 
 @Component({
   selector: 'app-grid',
@@ -10,18 +11,46 @@ export class GridComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.reform();
+  }
+
+  board: string[][];
+
+  playerA: boolean = true;
+
+  winner: string = null;
+  
+  add(n: number) {
+
+    for(var i=5; i>=0; i--) {
+      if(this.board[i][n] == null) {
+        this.board[i][n] = this.playerA ? '⭕️' : '❌';
+        this.check();
+        this.playerA = !this.playerA;
+        return;
+      }
+    }
+
+  }
+
+  check() {
+
   }
 
 
-  
+  reform() {
+    this.board = [
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null],
+    ];
 
-  board: string[][] = [
-    ['🈳', '❌', '🈳️', '⭕️', '🈳️', '🈳️', '🈳️', '❌'],
-    ['🈳️', '❌', '🈳️', '⭕️', '🈳️', '🈳️', '🈳️', '❌'],
-    ['🈳️', '❌', '❌', '⭕️', '🈳️', '🈳️', '🈳️', '❌'],
-    ['🈳️', '❌', '🈳️', '❌', '🈳️', '🈳️', '🈳️', '❌'],
-    ['🈳️', '❌', '🈳️', '⭕️', '❌', '🈳️', '🈳️', '❌'],
-    ['🈳️', '❌', '🈳️', '⭕️', '🈳️', '🈳️', '🈳️', '❌'],
-  ]
+    return;
+  }
+
+  
 
 }
